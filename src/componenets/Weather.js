72b9365e654393e2c56da4c5e37f9b4865d63b9f
Sogand-Hashemi <sogandhpz75@gmail.com>
2019-10-29 00:40:58 +0330

    commit all

--- src/componenets/Weather.js
+++ src/componenets/Weather.js
@@ -3,8 +3,8 @@ import React from 'react';
 const Weather = props => (
     <div>
         {props.city && props.country && <h3>Location: {props.city}, {props.country}</h3>}
-        {props.temperature && <h3>Temperature: {props.temperature} &#176;C</h3>}
-        {props.humidity && <h3>Humidity: {props.humidity}</h3> }
+        {props.temperature && <h3>Temperature: {props.temperature}&#176;C</h3>}
+        {props.humidity && <h3>Humidity: {props.humidity}&#37;</h3> }
         {props.description && <h3>Conditions: {props.description}</h3>}
         {props.error && <h3>{props.error}</h3>}
     </div>
