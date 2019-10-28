faf4219822d1ad89481b2d98cf2bddcdf7f2d0dc
Sogand-Hashemi <sogandhpz75@gmail.com>
2019-10-28 20:24:28 +0330

    commit all

--- src/componenets/Weather.js
+++ src/componenets/Weather.js
@@ -5,7 +5,7 @@ class Weather extends React.Component{
         return(
           <div>
               {this.props.city && this.props.country && <h3>Location: {this.props.city}, {this.props.country}</h3>}
-              {this.props.temperature && <h3>Temperature: {this.props.temperature}</h3>}
+              {this.props.temperature && <h3>Temperature: {this.props.temperature} &#176;C</h3>}
               {this.props.humidity && <h3>Humidity: {this.props.humidity}</h3> }
               {this.props.description && <h3>Conditions: {this.props.description}</h3>}
           </div>
